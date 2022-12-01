3e7df17fe92e790736bf6152233283577c31e747
mzdavid08 <dm.dmeraz.dm@gmail.com>
2022-11-30 17:38:49 -0700

    Maze walls and pellets are drawn onto map

--- evil_pacman.js
+++ evil_pacman.js
@@ -55,4 +55,44 @@ function generateMaze(game) {
     // Determine size of each maze element
     var height = game.height / maze.length;
     var width = game.width / maze[0].length;
+
+    // Get the game's 2D context
+    var context = game.getContext("2d");
+
+    // Draw the maze
+    for (let i = 0; i <= maze.length; i++) {
+        for (let j = 0; j <= maze[i].length; j++) {
+            switch (maze[i][j]) {
+                case '#':
+                    // Draw wall
+                    context.fillStyle = "#0000FF";
+                    context.fillRect(j * width, i * height, width, height);
+                    break;
+                case '.':
+                    // Draw normal pellet
+                    context.fillStyle = "white";
+                    context.beginPath();
+                    context.arc(j * width + (width / 2), i * height + (width / 2), height / 12, width, height);
+                    context.fill();
+                    context.closePath();    
+                    break;
+                case 'P':
+                    // Draw power pellet
+                    context.fillStyle = "#F3AFF1";
+                    context.beginPath();
+                    context.arc(j * width + (width / 2), i * height + (width / 2), height / 6, width, height);
+                    context.fill();
+                    context.closePath();    
+                    break;
+                case '!':
+                    // Draw poison pellet
+                    context.fillStyle = "#79EC74";
+                    context.beginPath();
+                    context.arc(j * width + (width / 2), i * height + (width / 2), height / 6, width, height);
+                    context.fill();
+                    context.closePath();    
+                    break;
+            }
+        }
+    }
 }
